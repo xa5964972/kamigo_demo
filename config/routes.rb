7280527e1241ca6177c8addd92a2eb9dd9a074e2
xa5964972 <93878560+xa5964972@users.noreply.github.com>
2021-11-08 12:22:56 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -16,39 +16,6 @@ Rails.application.routes.draw do
   
   get "test", to: "home#test"  #不能刪
 
-  # 查天氣
-  get "*location天氣", to: "weather#show"
-  get "(*location)天氣(*other)", to: "weather#show"
 
- 
-  
-
-  # 查詢當下的使用者
-  get "profile", to: "home#profile"
-
-  # flex message demo
-  get "flex", to: "flex#index"
-  get "flex/bubble_size_tests/:size", to: "flex#bubble_size_tests"
-  get "flex/apng_test", to: "flex#apng_test"
-  get "flex/500_buttons_demo", to: "flex#500_buttons_demo"
-
-  # LINE Flex Message Show Cases
-  get "flex/showcases", to: "flex_showcases#index"
-  get "flex/showcases/restaurant", to: "flex_showcases#restaurant"
-  get "flex/showcases/apparel", to: "flex_showcases#apparel"
-  get "flex/showcases/hotel", to: "flex_showcases#hotel"
-  get "flex/showcases/local_search", to: "flex_showcases#local_search"
-  get "flex/showcases/real_estate", to: "flex_showcases#real_estate"
-  get "flex/showcases/social", to: "flex_showcases#social"
-  get "flex/showcases/todo_app", to: "flex_showcases#todo_app"
-  get "flex/showcases/transit", to: "flex_showcases#transit"
-  get "flex/showcases/receipt", to: "flex_showcases#receipt"
-  get "flex/showcases/shopping", to: "flex_showcases#shopping"
-  get "flex/showcases/menu", to: "flex_showcases#menu"
-  get "flex/showcases/ticket", to: "flex_showcases#ticket"
-
-  # LIFF 分享好友
-  get "share_bot", to: "home#share_bot"
-  get "share_bot_flex", to: "home#share_bot_flex"
   
 end
